31808f5c8ad638a6d1baa698d6e319ab408afa71
Gustavo De Mari <guta302@hotmail.com>
2013-07-09 06:30:56 +0000

    Added index.html reading on web.js

--- web.js
+++ web.js
@@ -2,9 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var index = fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
-});
+  response.send(index.toString());
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
