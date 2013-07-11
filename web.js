ff7329efd6d147b5f06c901580354d892feaf75b
Gustavo De Mari <guta302@hotmail.com>
2013-07-11 03:10:00 +0000

    Grader.js and checks creation

--- web.js
+++ web.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var fs = require('fs');
 
-var app = express.createServer(express.logger());
+var app = express.createserver(express.logger());
 
-var index = fs.readFileSync('index.html').toString();
+var index = fs.readfilesync('index.html').toString();
 
 app.get('/', function(request, response) {
   response.send(index);
